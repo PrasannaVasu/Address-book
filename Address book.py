b2b33fd505f0d8bb48d1262ff419cc4be55416ec
Prasanna Vasudevan <prasanna261387@gmail.com>
2019-01-24 00:09:10 +0530

    Update Address book.py

--- Address book.py
+++ Address book.py
@@ -1,15 +1,25 @@
+
+def my_func(run):
+  option=raw_input("Do you want to continue Y/N:")
+  if option == "y" or option == "Y" :
+   return 6
+  else:
+    exit()
+
 run = True
 while run:
  print "ADDRESS BOOK"
  print "\n\t\t1.Create address book"
  print "\n\t\t2.Append address book"
- print "\n\t\t3.Quit"
+ print "\n\t\t3.View address book"
+ print "\n\t\t4.Quit"
  choice=input("\n\tEnter a choice:")
- while choice<4:
+ while choice<5:
         #get file choice from user
         if choice == 1:
             #create file
-            f= open("Address book.txt","w+")
+            a_name=raw_input("Enter the Address_book name:")
+            f= open(a_name + ".txt","w+")
             name=raw_input("Enter the name:")
             address=raw_input("Enter the address:")
             phone=input("Enter the Phone number:")
@@ -19,10 +29,11 @@ while run:
             f.write("\n\tPhone number:%d" % phone)
             f.write("\n\tMail id:%s" % mail)
             f.close() 
-            break
+            my_func(run)
         elif choice == 2:
             #append file
-            f= open("Address book.txt","a+")
+            a_name=raw_input("Enter the Address_book name to append:")
+            f= open(a_name + ".txt","a+")
             name=raw_input("Enter the name:")
             address=raw_input("Enter the address:")
             phone=input("Enter the Phone number:") 
@@ -32,9 +43,14 @@ while run:
             f.write("\n\tPhone number:%d" % phone)
             f.write("\n\tMail id:%s" % mail)
             f.close()
-            break
+            my_func(run)
         elif choice == 3:
+            #View file
+            a_name=raw_input("Enter the Address_book name to view:")
+            f= open(a_name + ".txt","r+")
+            print f.read()
+            f.close()
+            choice = my_func(run)
+        elif choice == 4:
             #exit
             exit()
-
- 
